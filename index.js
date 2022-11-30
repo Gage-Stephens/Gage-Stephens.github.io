2c7a9b2567b08b4eb3c0de15f9909b34325385a8
Gage-Stephens <112976757+Gage-Stephens@users.noreply.github.com>
2022-11-30 13:50:31 -0500

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-// document.getElementById("navAbout").addEventListener("click", () => {
-//     document.getElementById("carouselExampleIndicators").className="hidden";
-// });
-
-// document.getElementById("Home").addEventListener("click", () => {
-//     document.getElementById("carouselExampleIndicators").className="notHidden";
-//     document.getElementById("About").style.display="none";
-// });
-
